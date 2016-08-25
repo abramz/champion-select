49d22175589237da5af1e103005f6ac4a2f6025f
Andrew Shapro <andrew.shapro@gmail.com>
2016-08-24 17:42:30 -0700

    removes the news fetch & render from home
    - also fixes package.json (had "a" instead of my name in the author
    field)
    - also fix the eslintrc - allow console.log until I put in a logging lib
    - removes privacy content
    - adds a grid around the children in App

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -2,37 +2,26 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 
+// components
+import {
+  Col,
+  Row,
+} from 'react-bootstrap';
+
 const title = 'Champion Select';
 
-function Home({ news }, context) {
+function Home(props, context) {
   context.setTitle(title);
   return (
-    <div className={s.root}>
-      <div className={s.container}>
-        <h1 className={s.title}>React.js News</h1>
-        <ul className={s.news}>
-          {news.map((item, index) => (
-            <li key={index} className={s.newsItem}>
-              <a href={item.link} className={s.newsTitle}>{item.title}</a>
-              <span
-                className={s.newsDesc}
-                dangerouslySetInnerHTML={{ __html: item.contentSnippet }}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <Row>
+      <Col xs={12}>
+        <h1>Select your champion, Summoner!</h1>
+      </Col>
+    </Row>
   );
 }
 
-Home.propTypes = {
-  news: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    contentSnippet: PropTypes.string,
-  })).isRequired,
-};
+Home.propTypes = {};
 Home.contextTypes = { setTitle: PropTypes.func.isRequired };
 
 export default withStyles(s)(Home);
