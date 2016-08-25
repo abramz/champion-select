207c1369befd775228f996b8d6745af69f0ede27
Andrew Shapro <andrew.shapro@gmail.com>
2016-08-25 16:08:49 -0700

    refactor queries and add comments

--- src/data/queries/champion.js
+++ src/data/queries/champion.js
@@ -3,19 +3,19 @@ import ChampionType from '../types/ChampionType';
 import {
   idType,
   localeType,
-  optsType,
+  optionsType,
 } from './constants';
 
 /**
  * Champion Query
- * @type {{type: ChampionType, args: {id, locale, opts}, resolve: ((root, { id, locale, opts }:{id: *, locale: *, opts: *}))}}
+ * @type {{type: ChampionType, args: {id, locale, options}, resolve: ((root, { id, locale, options }:{id: *, locale: *, options: *}))}}
  */
 const champion = {
   type: ChampionType,
   args: {
     id: idType,
     locale: localeType,
-    opts: optsType,
+    options: optionsType,
   },
 
   /**
@@ -24,15 +24,15 @@ const champion = {
    * @param root - arguments passed to each request by the server
    * @param id - the id of the champion to retrieve
    * @param locale - the locale to retrieve this data for
-   * @param opts - what fields are we looking for
+   * @param options - what fields are we looking for
    * @returns {ChampionType}
    */
-  async resolve(root, { id, locale, opts }) {
+  async resolve(root, { id, locale, options }) {
     try {
       const lolApi = new LolApi();
-      return await lolApi.getChampion({ id, locale, opts });
+      return await lolApi.getChampion({ id, locale, options });
     } catch (error) {
-      console.log(id, locale, opts, error);
+      console.log(id, locale, options, error);
       throw new Error('Unable to retrieve champion id: "${id}".');
     }
   },
