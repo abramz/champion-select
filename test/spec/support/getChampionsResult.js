a4e80f5a45a27893ab0a5787e7488c7f16693bdb
Andrew Shapro <andrew.shapro@gmail.com>
2016-08-28 09:09:05 -0700

    adds unit tests for the champions query

---
+++ test/spec/support/getChampionsResult.js
@@ -0,0 +1,19 @@
+const withMap = {
+  data: {
+    a: { key: 'a', name: 'A' },
+    b: { key: 'b', name: 'B' },
+    c: { key: 'c', name: 'C' },
+    d: { key: 'd', name: 'D' },
+  },
+};
+
+const withArray = {
+  data: [
+    { key: 'a', name: 'A' },
+    { key: 'b', name: 'B' },
+    { key: 'c', name: 'C' },
+    { key: 'd', name: 'D' },
+  ],
+};
+
+export default { withMap, withArray };
