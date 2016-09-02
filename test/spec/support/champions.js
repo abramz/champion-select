2fdddbef5c355700b394568752622265888e9cf2
Andrew Shapro <andrew.shapro@gmail.com>
2016-09-02 10:42:04 -0700

    adds tests for the `VisibleChampionList` container

---
+++ test/spec/support/champions.js
@@ -0,0 +1,10 @@
+export default [
+  { key: 'A', name: 'Abcd', tags: ['a', 'b', 'c', 'd'] },
+  { key: 'a', name: 'abcd', tags: ['a', 'b', 'c', 'd'] },
+  { key: 'B', name: 'Bcde', tags: ['b', 'c', 'd', 'e'] },
+  { key: 'b', name: 'bcde', tags: ['b', 'c', 'd', 'e'] },
+  { key: 'C', name: 'Cdef', tags: ['c', 'd', 'e', 'f'] },
+  { key: 'c', name: 'cdef', tags: ['c', 'd', 'e', 'f'] },
+  { key: 'D', name: 'Defg', tags: ['d', 'e', 'f', 'g'] },
+  { key: 'd', name: 'defg', tags: ['d', 'e', 'f', 'g'] },
+];
