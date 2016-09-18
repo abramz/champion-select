b554e2d870a78e786d0facdf64b679b190d8ac30
Andrew Shapro <andrew.shapro@gmail.com>
2016-09-17 21:33:58 -0700

    remove commented out code

--- test/spec/components/IconListItem.spec.js
+++ test/spec/components/IconListItem.spec.js
@@ -60,31 +60,4 @@ describe('<IconListItem />', () => {
     const textRef = wrapper.find('div').at(2);
     expect(textRef.text()).to.equal('Abcd');
   });
-
-
-  // let wrapper;
-  // const baseUrl = '//abcdef.gg';
-  // const version = '1.1.1';
-  //
-  // beforeEach(() => {
-  //   wrapper = shallow(<UnstyledIconListItem cdnUrl={baseUrl} champion={champion} version={version} />);
-  //   expect(wrapper).to.have.exactly(2).descendants(Link);
-  // });
-  //
-  // it('should display an image of the champion', () => {
-  //   const ref = wrapper.find(Link).first();
-  //   expect(ref).to.have.prop('to', '/champion/A');
-  //   expect(ref).to.have.exactly(1).descendants('img');
-  //
-  //   const imgRef = ref.find('img');
-  //   expect(imgRef).to.have.prop('src', `${baseUrl}/1.1.1/img/group/A.png`);
-  //   expect(imgRef).to.have.prop('alt', 'Abcd');
-  //   expect(imgRef).to.have.prop('role', 'presentation');
-  // });
-  //
-  // it('should display the name of the champion', () => {
-  //   const nameRef = wrapper.find(Link).last();
-  //   expect(nameRef).to.have.prop('to', '/champion/A');
-  //   expect(nameRef).to.have.prop('children', 'Abcd');
-  // });
 });
